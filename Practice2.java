a66faffc67b980c58c19f7a2ba4dc4bbee88759e
patelrudresh <rudreshpatel728@gmail.com>
2025-05-24 19:22:43 +0530

    Add files via upload

---
+++ Practice2.java
@@ -0,0 +1,48 @@
+package com.stringHandlling;
+
+import java.util.Scanner;
+
+public class Practice2 {
+public static void main(String[] args) {
+	//que8 - Write a program to find a substring "hari" in the given string.
+      Scanner sc = new Scanner(System.in);
+//      
+      System.out.println("enter the name");
+      String name =sc.nextLine();
+//      if(name.contains("hari")) {
+//    	  System.out.println("hari is available");      }
+//      else
+//    	  System.out.println("hari is not available");
+      
+//      que 9    Rewrite above program to print "hari is available"
+//      when the given string has "hari" in any case.
+//      if(name.toLowerCase().contains("hari")) {
+//    	  System.out.println("hari is available");      }
+//      else
+//    	  System.out.println("hari is not available");
+      
+//      Que 11.     Rewrite above program to print "hari is available"
+//      if the given string contains characters 'h' 'a' 'r' 'i' in any
+//      sequence at any place by ignoring case.
+     
+//      if(name.contains("h")&& name.contains("a")&& name.contains("r")&& name.contains("i")) 
+//    	  System.out.println("hari is available");     
+//    else
+//  	  System.out.println("hari is not available");
+      
+      
+//      P12.
+//      Rewrite above program to print "hari is available"
+//      only if the given string contains  characters 'h' 'a' 'r' 'i' in the
+//      same sequence at any place, ignore case.
+//          Input:
+//              hi h1234a4321rkjlkjlki hru?
+//         
+//          output:
+//              hari is available
+//      
+       String target ="hari";
+      
+
+}
+}
