a44ec0c8d5734b3094b15b315c8bdd324023e2e1
patelrudresh <rudreshpatel728@gmail.com>
2025-05-27 21:56:37 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+package com.generics;
+
+class Example<T> {  //Generic parameter class
+	void m1(T i) {	  //Generic parameter method
+		System.out.println(i.getClass().getSimpleName() + " type value passed");
+	}
+	
+	static <T> void m2(T t) {	//static generic method
+		System.out.println(t.getClass().getSimpleName() + " type value passed");
+	}
+}
